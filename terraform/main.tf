46025e30bd0e995a471d6e9a3e517d977282528d
jakkalilokesh <jakkalilokesh@gmail.com>
2026-07-11 17:05:10 +0530

    fix(terraform): remove default kubernetes provider to prevent plan-time EKS connection errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,16 +23,6 @@ provider "kubernetes" {
   alias = "dummy"
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
-
 # VPC Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
